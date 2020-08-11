273eeadb56c851e6204770a50c6659dd357a8558
Mulcahy <Paul.Mulcahy@fujitsu.com>
2020-08-11 09:31:01 -0700

    Chapter 1 - Question 4 - Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase that is the same forwards and backwards. A permutation is a rearrangement of letters. The palindrome does not need to be limited to just dictionary words.

---
+++ src/test/java/ca/pmulcahy/ctci7/chp1/TestQ4_PalindromePermutation.java
@@ -0,0 +1,63 @@
+package ca.pmulcahy.ctci7.chp1;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TestQ4_PalindromePermutation{
+	
+	@Test
+	public void emptyString() {
+		
+		final String empty = "";
+				
+		boolean result = Q4_PalindromePermutation.isPermutationOfPalindrome(empty);
+		assertTrue(result, empty + " is a permutation of a palindrome: /'/'");
+	}
+	
+	@Test
+	public void singleWhiteSpaceString() {
+		
+		final String whiteSpace = " ";
+				
+		boolean result = Q4_PalindromePermutation.isPermutationOfPalindrome(whiteSpace);
+		assertTrue(result, whiteSpace + " is a permutation of a palindrome: /'/'.");
+	}
+
+	@Test
+	public void tripleLetterString() {
+		
+		final String possiblePermutation = "ababa  ";
+				
+		boolean result = Q4_PalindromePermutation.isPermutationOfPalindrome(possiblePermutation);
+		assertTrue(result, possiblePermutation + " is a permutation of a palindrome: /'  /'.");
+	}
+	
+	@Test
+	public void tact_Coa() {
+		
+		final String tact_Coa = "Tact Coa";
+				
+		boolean result = Q4_PalindromePermutation.isPermutationOfPalindrome(tact_Coa);
+		assertTrue(result, tact_Coa + " is a permutation of a palindrome: /'taco cat/', /'atco cta/', etc.");
+	}	
+	
+	@Test
+	public void notPalindromePermutation() {
+		
+		final String notPalindromePermutation = "ab";
+				
+		boolean result = Q4_PalindromePermutation.isPermutationOfPalindrome(notPalindromePermutation);
+		assertFalse(result, notPalindromePermutation + " is not a permutation of a palindrome.");
+	}
+
+	@Test
+	public void notPalindromePermutationTripleLeters() {
+		
+		final String notPalindromePermutation = "aaabbb";
+				
+		boolean result = Q4_PalindromePermutation.isPermutationOfPalindrome(notPalindromePermutation);
+		assertFalse(result, notPalindromePermutation + " is not a permutation of a palindrome.");
+	}
+	
+}
